3280fdc4ae36f69d20c0272aabc3e1e0fddc1472
mistrjirka <jirka.svitil@gmail.com>
2018-12-29 11:41:14 +0100

    starting on game

--- js/game.ts
+++ js/game.ts
@@ -1,198 +1,66 @@
 //references
 /// <reference path = "engine.ts" />
 
-window.onload = () => {
-    var player = {};
-    let game = new KartiskyGL(
-        "game",
-        "auto",
-        [
-            {
-                name: "car",
-                URL: "obr/car.png"
-            }
-        ],
-        [
-            /*{
-                value: player,
-                name: "ahoj",
-                anchor: [0.2, 0.2]
-            }*/
-        ]
-    );
-
-    var sprite = {};
-
-    /*    setTimeout(() => game.load([{
-            URL: "obr/ahoj.png",
-            name: "nien"
-        }], function () {
-            game.createSprite([{
-                name: "nien",
-                value: sprite,
-                anchor: [0.5, 0.5]
-            }]);
-        }), 1000);*/
-    let exampleMap: IMap_for_rendering = {
-        map: [
-            [
-                {
-                    empty: false,
-                    card: {
-                        sprite: {
-                            name: "car",
-                            anchor: [0, 0],
-                            typeOfScale: "automatic"
-                        },
-                        statistics: {}
-                    }
-                },
-                {
-                    empty: true
-                },
-                {
-                    empty: true
-                }
-            ],
-            [
-                {
-                    empty: true
-                },
-                {
-                    empty: true
-                },
-                {
-                    empty: false,
-                    card: {
-                        sprite: {
-                            name: "car",
-                            anchor: [0.1, 0.1],
-                            typeOfScale: "automatic"
-                        },
-                        statistics: {}
-                    }
-                }
-            ]
-        ],
-        x: 0,
-        y: 0,
-        x_size: 100,
-        y_size: 100,
-        background: {
-            anchor: [1, 1],
-            autoScale: true,
-            x: 0,
-            y: 0,
-            name: "redBackground"
-        },
-        id: "map2"
+interface ICardConfig {
+    width: number;
+    height: number;
+    hero: {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
     };
-    setTimeout(function() {
-        game.load(
-            [
-                {
-                    URL: "obr/background.png",
-                    name: "redBackground",
-                    type: "image"
-                }
-            ],
-            function() {
-                var map = new KartiskyGL.Map(game.game, exampleMap);
-                setTimeout(function() {
-                    map.addToMap([
-                        {
-                            sprite: {
-                                name: "car",
-                                anchor: [0.1, 0.1],
-                                typeOfScale: "automatic"
-                            },
-                            x: 0,
-                            y: 1
-                        },
-                        {
-                            sprite: {
-                                name: "car",
-                                anchor: [0.1, 0.1],
-                                typeOfScale: "automatic"
-                            },
-                            x: 0,
-                            y: 2
-                        }
-                    ]);
-                    map.removeFromMap([{ x: 0, y: 0 }]);
-                    var cardBox = new KartiskyGL.Cardbox(game.game, {
-                        x: 300,
-                        y: 400,
-                        width: 200,
-                        height: 400,
-                        anchor: [0, 0],
-                        card_size: {
-                            width: 100,
-                            height: 100
-                        },
-                        overlapHeight: 2,
-                        overlapWidth: 2,
-                        background: {
-                            anchor: [1, 1],
-                            autoScale: true,
-                            x: 300,
-                            y: 400,
-                            name: "redBackground"
-                        },
-                        numOfCardsX: 2,
-                        numOfCardsY: 3
-                    });
+    text: {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+    };
+    name: string;
+}
 
-                    cardBox.addToCardBox({
-                        name: "car",
-                        anchor: [0, 0],
-                        scale: {
-                            width: 100,
-                            height:100
-                        }
-                    });
-                    cardBox.addToCardBox({
-                        name: "car",
-                        anchor: [0, 0],
-                        scale: {
-                            width: 100,
-                            height:100
-                        }
-                    });
-                    cardBox.addToCardBox({
-                        name: "car",
-                        anchor: [0, 0],
-                        scale: {
-                            width: 100,
-                            height:100
-                        }
-                    });
-                    cardBox.addToCardBox({
-                        name: "car",
-                        anchor: [0, 0],
-                        scale: {
-                            width: 100,
-                            height:100
-                        }
-                    });
-                    cardBox.addToCardBox({
-                        name: "car",
-                        anchor: [0, 0],
-                        scale: {
-                            width: 100,
-                            height:100
-                        }
-                    });
-                    cardBox.addToCardBox({
-                        name: "car",
-                        anchor: [0, 0],
-                        scale: {
-                            width: 100,
-                            height:100
-                        }
-                    });
-                }, 2000);
-            }
-        );
-    }, 2000);
-};
+interface IText extends Sprite{
+
+}
+
+interface ICardAssemblerPartsKnown {
+    type: "hero" | "text";
+    sprite: Phaser.Sprite | Sprite | Phaser.Text;
+}
+
+interface ICardAssemblerPartsUnknown {
+    type: "other";
+    sprite: {
+        name: string;
+        anchor: number[];
+        x: number;
+        y: number;
+        scale?: {
+            width: number;
+            height: number;
+        };
+    };
+}
+
+class Game extends KartiskyGL {
+    constructor(
+        div: string,
+        toLoad: ISprite_loading = [],
+        ToCreate: ISprite_create = [],
+        resolution: { x: number; y: number } = { x: 1280, y: 720 }
+    ) {
+        super(div, "auto", toLoad, ToCreate, resolution.x, resolution.y);
+    }
+
+    static cardAssembler = class extends KartiskyGL.Render{
+        public hero: Phaser.Sprite;
+        public text: Phaser.Text;
+        public other: (Phaser.Sprite | Phaser.Text | Phaser.Image)[];
+
+        constructor(game: Phaser.Game, parts: (ICardAssemblerPartsKnown | ICardAssemblerPartsUnknown) [], cardConfig: ICardConfig, x: number, y: number, width: number, height: number) {
+            super(game);
+
+
+        }
+    };
+}
